c60a45ea0c2c6383d18281fe39ad6d71021199cd
Mitul Garg <mitul7hero@gmail.com>
2023-07-20 12:00:20 +0530

    Major Card styling improvements

--- src/components/TempleCard.js
+++ src/components/TempleCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import './TempleCard.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { CardDeck } from 'reactstrap';
 import { Link } from 'react-router-dom'
 // import Description from "./description";
 
@@ -11,19 +12,23 @@ const TempleCard = ({ id, name, location, image, description }) => {
   console.log(id, name, location, image, description);
   return (
 
-    <div>
-      <Card style={{ width: '20rem' }}>
-        <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{location}</Card.Text>
-          <Button variant="primary">
-            <Link className=" text-white text-decoration-none" to={`/temples/${id}`}>Go somewhere</Link>
-            </Button>
-        </Card.Body>
-      </Card>
+ <CardDeck>
+      <div className="temple-card">
+          <Card style={{width:"20rem", height:"20rem"}}>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+              <Card.Title>{name}</Card.Title>
+              <Card.Text>{location}</Card.Text>
+              <div class="button-position">
+              <Button variant="primary">
+                <Link className=" text-white text-decoration-none" to={`/temples/${id}`}>Go somewhere</Link>
+              </Button>
+              </div>
+            </Card.Body>
+          </Card>
+      </div> 
+  </CardDeck>
 
-    </div>
   );
 };
 
